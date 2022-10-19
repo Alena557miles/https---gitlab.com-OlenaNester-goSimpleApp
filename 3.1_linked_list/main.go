d0e26e433c00515490b4fb26ed30b0bfa5810673
Alena557miles <557miles@gmail.com>
2022-10-19 21:31:04 +0300

    added hw3.1 linked list

--- 3.1_linked_list/main.go
+++ 3.1_linked_list/main.go
@@ -11,13 +11,15 @@ type linkedList struct {
 	lenght int
 }
 
-func (l *linkedList) append(n *node) {
+// ADD NODE TO THE LIST
+func (l *linkedList) appendNode(n *node) {
 	second := l.head
 	l.head = n
 	l.head.next = second
 	l.lenght++
 }
 
+// PRINT LIST
 func (l linkedList) printListNodes() {
 	toPrint := l.head
 	for l.lenght != 0 {
@@ -28,6 +30,26 @@ func (l linkedList) printListNodes() {
 
 }
 
+// DELETE NODE BY VALUE
+func (l *linkedList) deleteNodebyValue(v string) {
+	if l.lenght == 0 {
+		return
+	}
+	if l.head.value == v {
+		l.head = l.head.next
+		l.lenght--
+	}
+	previousToDelete := l.head
+	for previousToDelete.next.value != v {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.lenght--
+}
+
 func main() {
 	list := linkedList{}
 	node1 := &node{value: "first"}
@@ -35,11 +57,18 @@ func main() {
 	node3 := &node{value: "third"}
 	node4 := &node{value: "fourth"}
 	node5 := &node{value: "fifth"}
-	list.append(node1)
-	list.append(node2)
-	list.append(node3)
-	list.append(node4)
-	list.append(node5)
+
+	// ADD NODE
+	list.appendNode(node1)
+	list.appendNode(node2)
+	list.appendNode(node3)
+	list.appendNode(node4)
+	list.appendNode(node5)
 	fmt.Println(list)
+	list.printListNodes()
+	fmt.Println()
+
+	// DELETE NODE BY VALUE
+	list.deleteNodebyValue("fifth")
 	list.printListNodes()
 }
