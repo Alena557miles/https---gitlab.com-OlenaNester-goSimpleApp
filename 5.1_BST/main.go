ef28c1eb4b23ca200110586f0c1799c5afa6dc2e
Alena557miles <557miles@gmail.com>
2022-10-31 23:04:38 +0200

    added hw5.1, hw 5.2

---
+++ 5.1_BST/main.go
@@ -0,0 +1,159 @@
+package main
+
+import "fmt"
+
+// BST - search, insert, delete
+
+type bstNode struct {
+	value int
+	left  *bstNode
+	right *bstNode
+}
+
+type bst struct {
+	root *bstNode
+}
+
+func (b *bst) Insert(value int) {
+	b.InsertRec(b.root, value)
+}
+
+func (b *bst) InsertRec(node *bstNode, value int) *bstNode {
+	if b.root == nil {
+		b.root = &bstNode{value: value}
+		return b.root
+	}
+	if node == nil {
+		return &bstNode{value: value}
+	}
+	if value <= node.value {
+		node.left = b.InsertRec(node.left, value)
+	}
+	if value > node.value {
+		node.right = b.InsertRec(node.right, value)
+	}
+	return node
+}
+
+func minValued(b *bstNode) *bstNode {
+	temp := b
+	for nil != temp && temp.left != nil {
+		temp = temp.left
+	}
+	return temp
+}
+func (b *bst) Delete(value int) {
+	b.DeleteNode(b.root, value)
+}
+func (b *bst) DeleteNode(node *bstNode, value int) *bstNode {
+	if node == nil {
+		return b.root
+	}
+	if value < node.value {
+		node.left = b.DeleteNode(node.left, value)
+	} else if value > node.value {
+		node.right = b.DeleteNode(node.right, value)
+	} else if value == node.value {
+		if node.left == nil && node.right == nil {
+			node = nil
+			return node
+		}
+		if node.left == nil && node.right != nil {
+			temp := node.right
+			node = nil
+			node = temp
+			return node
+		}
+		if node.left != nil && node.right == nil {
+			temp := node.left
+			node = nil
+			node = temp
+			return node
+		}
+		tempNode := minValued(node.right)
+		node.value = tempNode.value
+		node.right = b.DeleteNode(node.right, tempNode.value)
+	}
+	return b.root
+}
+
+func (b *bst) Search(value int) error {
+	node := b.SearchNode(b.root, value)
+	if node == nil {
+		return fmt.Errorf("Value: %d is not exist in a tree", value)
+	}
+	return nil
+}
+
+func (b *bst) SearchNode(node *bstNode, value int) *bstNode {
+	if node == nil {
+		return nil
+	}
+	if node.value == value {
+		return b.root
+	}
+	if value < node.value {
+		return b.SearchNode(node.left, value)
+	}
+	return b.SearchNode(node.right, value)
+}
+
+func (b *bst) inorder() {
+	b.inorderRec(b.root)
+	fmt.Println("b.root.value", b.root.value)
+}
+
+func (b *bst) inorderRec(node *bstNode) {
+	if node != nil {
+		b.inorderRec(node.left)
+		fmt.Println(node.value)
+		b.inorderRec(node.right)
+	}
+}
+
+func (b *bst) reset() {
+	b.root = nil
+}
+
+func main() {
+	bst := &bst{}
+	e := []int{8, 3, 70}
+	for _, val := range e {
+		bst.Insert(val)
+	}
+	fmt.Printf("Printing Inorder:\n")
+	bst.inorder()
+	bst.reset()
+
+	// eg := []int{4, 5, 7, 6, -1, 99, 5}
+	// for _, val := range eg {
+	// 	bst.Insert(val)
+	// }
+	// fmt.Printf("\nPrinting Inorder:\n")
+	// bst.inorder()
+	// fmt.Printf("\nFinding Values:\n")
+	// err := bst.Search(7)
+	// if err != nil {
+	// 	fmt.Printf("Value %d Not Found\n", 7)
+	// } else {
+	// 	fmt.Printf("Value %d Found\n", 7)
+	// }
+	// err = bst.Search(6)
+	// if err != nil {
+	// 	fmt.Printf("Value %d Not Found\n", 6)
+	// } else {
+	// 	fmt.Printf("Value %d Found\n", 6)
+	// }
+	// err = bst.Search(5)
+	// if err != nil {
+	// 	fmt.Printf("Value %d Not Found\n", 5)
+	// } else {
+	// 	fmt.Printf("Value %d Found\n", 5)
+	// }
+	// err = bst.Search(1)
+	// if err != nil {
+	// 	fmt.Printf("Value %d Not Found\n", 1)
+	// } else {
+	// 	fmt.Printf("Value %d Found\n", 1)
+	// }
+}
---
+++ 5.2_HB/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"hash/fnv"
+)
+
+const MAP_SIZE = 10
+
+type Node struct {
+	key   string
+	value string
+	next  *Node
+}
+
+type HashMap struct {
+	Data []*Node
+}
+
+func NewDict() *HashMap {
+	return &HashMap{Data: make([]*Node, MAP_SIZE)}
+}
+
+func (n *Node) String() string {
+	return fmt.Sprintf("<Key: %s, Value: %s>\n", n.key, n.value)
+}
+
+func (h *HashMap) String() string {
+	var output bytes.Buffer
+	fmt.Fprintln(&output, "{")
+	for _, n := range h.Data {
+		if n != nil {
+			fmt.Fprintf(&output, "\t%s: %s\n", n.key, n.value)
+			for node := n.next; node != nil; node = node.next {
+				fmt.Fprintf(&output, "\t%s: %s\n", node.key, node.value)
+			}
+		}
+	}
+
+	fmt.Fprintln(&output, "}")
+
+	return output.String()
+}
+
+func (h *HashMap) Insert(key string, value string) {
+	i := getIndex(key)
+	if h.Data[i] == nil {
+		h.Data[i] = &Node{key: key, value: value}
+	} else {
+		newNode := h.Data[i]
+		for ; newNode.next != nil; newNode = newNode.next {
+			if newNode.key == key {
+				newNode.value = value
+				return
+			}
+		}
+		newNode.next = &Node{key: key, value: value}
+	}
+}
+
+func (h *HashMap) Get(key string) (string, bool) {
+	i := getIndex(key)
+	if h.Data[i] != nil {
+		node := h.Data[i]
+		for ; ; node = node.next {
+			if node.key == key {
+				return node.value, true
+			}
+			if node.next == nil {
+				break
+			}
+		}
+	}
+	return "", false
+}
+
+func hash(key string) (hash uint32) {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return h.Sum32()
+}
+
+func getIndex(key string) (index int) {
+	return int(hash(key)) % MAP_SIZE
+}
+
+func main() {
+	a := NewDict()
+	a.Insert("name", "ishan")
+	a.Insert("gender", "male")
+	a.Insert("city", "mumbai")
+	a.Insert("lastname", "khare")
+	if value, ok := a.Get("name"); ok {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Value did not match!")
+	}
+
+	fmt.Println(a)
+}
